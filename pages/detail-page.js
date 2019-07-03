6ff6f2921b6d033d441a17b58d25c669b1525a77
Diana <dianadiasds@gmail.com>
2019-07-03 17:45:31 +0200

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,15 +1,37 @@
+function doesNotPassAllValidations(name , msg) {
+    if(!name) {
+        alert ('Please, fill name') 
+        return true
+    }
+    if(!msg) {
+        alert ('Please, fill the message') 
+        return true
+    }
+    if(msg.length > 280) {
+        alert('Your comment is too long')
+        return true
+    }          
+    return false
+
+}
+
 function submitComment() {
+
     const inputField = document.getElementById('name')
     const name = inputField.value
     const textArea = document.getElementById('msg')
     const msg = textArea.value
 
+    if(doesNotPassAllValidations(name, msg)){
+        return null
+    }
+
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p') 
 
-    h3.innerHTML = `${name} said:`
-    p.innerHTML = msg
+    h3.innerHTML = `${name.toUpperCase()} said:`
+    p.innerHTML = `"${msg}"` 
     comment.classList.add('comment')
     comment.appendChild(h3)
     comment.appendChild(p)
@@ -19,5 +41,5 @@ function submitComment() {
 
     inputField.value = null
     textArea.value = null
-
 }
+
